157ea869421fea4c0ab111a4a6a7bb625e91c6b9
BosEriko <bos@eriko.ph>
2026-02-16 08:49:48 +0800

    Feature: use If block on welcome message

--- .files/modules/welcome/config.z.sh
+++ .files/modules/welcome/config.z.sh
@@ -1,7 +1,13 @@
 # ========================================================================== [Configuration] ===== #
 
-case "$OS_TYPE" in
-mac) echo "Welcome to your macOS shell!" ;;
-win) echo "Welcome to your Windows shell!" ;;
-stm) echo "Welcome to your SteamOS shell!" ;;
-esac
+if [[ "$OS_TYPE" == "mac" ]]; then
+  echo "Welcome to your macOS shell!"
+fi
+
+if [[ "$OS_TYPE" == "win" ]]; then
+  echo "Welcome to your Windows shell!"
+fi
+
+if [[ "$OS_TYPE" == "stm" ]]; then
+  echo "Welcome to your SteamOS shell!"
+fi
